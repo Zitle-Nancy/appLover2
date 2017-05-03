8bf1d53f2f61e2af2a814996d6e00e005f7b7a44
nancyzj <ancizj@hotmail.com>
2017-05-03 16:55:33 -0500

    pagina funcional

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ var imagenJs = document.getElementsByClassName('imagen-js');
+ var imprimirImagen = document.getElementById('imprimir-imagen');
+ var cerrar = document.getElementById('cerrar');
+ cerrar.addEventListener('click',cerrarImg);
+ var longitud = imagenJs.length;
+ console.log(imagenJs);
+ 
+
+ for(var i=0; i< longitud; i++){
+ 	imagenJs[i].addEventListener('click',mostrar);
+ }
+
+ function mostrar(){
+ 	var imgModal = document.getElementById('img-modal');
+	imprimirImagen.style.display = "block";
+	// aqui this es imagenJs
+	//children da un arreglo de sus hijos
+	//this de este children su hijo [0], indice del que querremos y obtenemos la src
+	imgModal.src = this.children[0].src;	
+ }
+function cerrarImg(){
+	imprimirImagen.style.display = "none";
+}
